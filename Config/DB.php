433f040bc185da33dee182a71b6f91919cefa731
snel <fransnellagirousse@gmail.com>
2023-11-08 08:57:14 +0300

    first commit

---
+++ Config/DB.php
@@ -0,0 +1,11 @@
+<?php
+//element necessaire a ma base de donnee
+
+
+namespace Config;
+class DB {
+CONST USERNAME = 'suividevol';
+CONST NAME = 'suividevol';
+CONST PASSWORD = 'suividevol';
+CONST HOST = 'localhost'; 
+}
---
+++ app/Models/Pays.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace app\Models;
+
+use Kernel\Model;
+class pays extends Model {
+protected static string $table = 'pays';
+}
